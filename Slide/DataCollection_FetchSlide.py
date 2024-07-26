b9b5e0cd13001c93ac5f50e41531045a6d1cbcb3
YujieZhu7 <156827930+YujieZhu7@users.noreply.github.com>
2024-07-26 23:20:28 +0800

    Update DataCollection_FetchSlide.py

--- Slide/DataCollection_FetchSlide.py
+++ Slide/DataCollection_FetchSlide.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 import random
 import pickle
+import sys
 import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, parent_dir)
