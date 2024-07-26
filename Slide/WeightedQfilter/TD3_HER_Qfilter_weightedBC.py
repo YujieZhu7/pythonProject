eec71b281781be770db47a81a3a31272bd9c1c31
YujieZhu7 <156827930+YujieZhu7@users.noreply.github.com>
2024-07-26 22:28:20 +0800

    Update TD3_HER_Qfilter_weightedBC.py

--- Slide/WeightedQfilter/TD3_HER_Qfilter_weightedBC.py
+++ Slide/WeightedQfilter/TD3_HER_Qfilter_weightedBC.py
@@ -258,8 +258,8 @@ class Agent(object):
                 num_accept = mask.sum(dim=0)[0].detach().cpu().item()
                 num_total = self.batch_size_demo
                 mask2 = torch.ge(Q_dem, demos_Q)
-                Q_dem_subset = Q_dem_set[mask2]
-                demos_Q_subset = demos_Q_set[mask2]
+                Q_dem_subset = Q_dem[mask2]
+                demos_Q_subset = demos_Q[mask2]
                 Qdiff = Q_dem_subset - demos_Q_subset
                 meanQdiff = Qdiff.sum()/num_accept
                 weights = Qdiff/(meanQdiff*num_accept)
