bb21a24e659e9397ec0fad1eef4b8af7473cafd7
YujieZhu7 <156827930+YujieZhu7@users.noreply.github.com>
2024-07-29 00:27:53 +0800

    Update TD3_HER_weightedBC_only2.py

--- Qfilter_HER/Algo/TD3_HER_weightedBC_only2.py
+++ Qfilter_HER/Algo/TD3_HER_weightedBC_only2.py
@@ -186,7 +186,7 @@ class Agent(object):
                 squared_errors = (demos_policy_actions - demos_action)**2
                 weighted_squared_errors = weights * squared_errors
                 BC_loss = weighted_squared_errors.sum()
-                print(BC_loss.shape)
+                print(BC_loss)
                 # print(weights.sum())
 
                 # BC_loss = F.mse_loss(demos_policy_actions, demos_action)
